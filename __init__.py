fe5a49b4698e360274bd6ee5988d02bb49811edd
Fredrik Erlandsson <fredrik.e@gmail.com>
2019-03-12 10:47:03 +0100

    added tests

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""Pytfiac, Python3 library for tfiac units."""
+
+import sys
+import logging
+
+assert sys.version_info >= (3, 0)
+
+_LOGGER = logging.getLogger(__name__)
---
+++ test/mock_tfiac.py
@@ -0,0 +1,63 @@
+"""Test client capabilities."""
+import asyncio
+import socket
+
+from tellsticknet.util import (sock_recvfrom, sock_sendto)
+import pytfiac
+
+
+STATUSUPDATEMSG = """<msg msgid="statusUpdateMsg" type="Control" seq="555">
+  <statusUpdateMsg>
+    <BaseMode>selfFeel</BaseMode>
+    <TurnOn>on</TurnOn>
+    <Opt_ECO>off</Opt_ECO>
+    <Opt_super>off</Opt_super>
+    <SetTemp>77</SetTemp>
+    <WindSpeed>Low</WindSpeed>
+    <Degree_Half>off</Degree_Half>
+    <Opt_healthy>off</Opt_healthy>
+    <Opt_antiMildew>off</Opt_antiMildew>
+    <Opt_StereoWind>off</Opt_StereoWind>
+    <Opt_heating>on</Opt_heating>
+    <Countdown_Timer_Off>00:00</Countdown_Timer_Off>
+    <Countdown_Timer_On>00:00</Countdown_Timer_On>
+    <Infrared_Direct>off</Infrared_Direct>
+    <Infrared_TurnOn>off</Infrared_TurnOn>
+    <HumidityEnable>off</HumidityEnable>
+    <CleannessEnable>off</CleannessEnable>
+    <WindDirection_H>off</WindDirection_H>
+    <WindDirection_V>off</WindDirection_V>
+    <IndoorTemp>70</IndoorTemp>
+    <Opt_sleepMode>off:0:0:0:0:0:0:0:0:0:0</Opt_sleepMode>
+    <OutdoorTemp>0</OutdoorTemp>
+    <BeepEnable>on</BeepEnable>
+    <DeviceName>Katie AC</DeviceName>
+  </statusUpdateMsg>
+</msg>
+"""
+
+loop = asyncio.get_event_loop()
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.setblocking(False)
+
+host = 'localhost'
+port = pytfiac.UDP_PORT
+
+sock.bind((host, port))
+
+
+async def mock_tfiac(sock):
+    """Mock tfiac unit."""
+    while True:
+        data, addr = await sock_recvfrom(sock, 2048)
+        if 'msgid="SyncStatusReq"' in data.decode():
+            data = STATUSUPDATEMSG.encode()
+        await sock_sendto(sock, data, addr)
+
+if __name__ == "__main__":
+    try:
+        loop.run_until_complete(mock_tfiac(sock))
+    finally:
+        loop.close()
